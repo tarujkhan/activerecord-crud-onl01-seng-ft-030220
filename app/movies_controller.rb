0a4d6c88f4393e88fb5a8c41924bcf719728ceb8
taroj khan <tarujkhan@yahoo.com>
2020-04-09 00:01:50 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -63,7 +63,7 @@ def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
   Movie.where("release_date > 2002").order
-(:release_date desc)
+(:release_date DESC)
 end
 
 def can_be_found_updated_and_saved
@@ -97,12 +97,12 @@ def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
   movies = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
   movie.destroy
-  __
+  
 end
 
 def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
-  __
+  Movie.delete_all
 end
